48dd84329b4d4837efaebb90a8f43f650aef81af
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-06-07 02:43:48 +0200

    fix: prettier

--- Task 1/server/models/User.js
+++ Task 1/server/models/User.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const usersPath = path.join(__dirname, '..', '..','data', 'users.json')
+const usersPath = path.join(__dirname, '..', '..', 'data', 'users.json')
 
 class User {
 
@@ -56,8 +56,8 @@ class User {
 
     // Check for non-empty phone
     if (!user.phone) {
-        throw new Error('Phone is required')
-      }
+      throw new Error('Phone is required')
+    }
 
     const newUser = {
       id: uuid(),
@@ -78,9 +78,9 @@ class User {
     if (userIndex === -1) {
       throw new Error('User not found')
     }
-  
+
     const existingUser = users[userIndex]
-  
+
     // Check for unique email (excluding the current user)
     const isEmailTaken = users.some(
       (existingUser) => existingUser.email === updatedUser.email && existingUser.id !== id
@@ -88,7 +88,7 @@ class User {
     if (isEmailTaken) {
       throw new Error('Email already exists')
     }
-  
+
     // Check for unique phone (excluding the current user)
     const isPhoneTaken = users.some(
       (existingUser) => existingUser.phone === updatedUser.phone && existingUser.id !== id
@@ -96,19 +96,31 @@ class User {
     if (isPhoneTaken) {
       throw new Error('Phone already exists')
     }
-  
+
     // Concatenate existing user data with updated fields
     const updatedUserData = {
-        id: existingUser.id,
-        firstName: updatedUser.firstName || existingUser.firstName,
-        lastName: updatedUser.lastName || existingUser.lastName,
-        email: updatedUser.email || existingUser.email,
-        phone: updatedUser.phone || existingUser.phone,
-      }
-  
+      id: existingUser.id,
+      firstName: updatedUser.firstName || existingUser.firstName,
+      lastName: updatedUser.lastName || existingUser.lastName,
+      email: updatedUser.email || existingUser.email,
+      phone: updatedUser.phone || existingUser.phone,
+    }
+
     users[userIndex] = updatedUserData;
     fs.writeFileSync(usersPath, JSON.stringify(users, null, 2))
     return updatedUserData
+  }
+
+  static deleteUser(id) {
+    const users = User.getAllUsers();
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      throw new Error('User not found')
+    }
+
+    const deletedUser = users.splice(userIndex, 1)[0];
+    fs.writeFileSync(usersPath, JSON.stringify(users, null, 2));
+    return deletedUser;
   }
 }
 
